0fede2cf28ecb2c2528ef1552d557129b23d81b0
guibrbs <200037994@aluno.unb.br>
2022-11-18 21:39:52 -0300

    style: add unb logo and a font type

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
+import unbLogo from './assets/logo_unb.png'
 
 function App() {
   return (
     <div className="App">
-      <h1>PA 2022<span /></h1>
+      <h1>PA 2022 <img src={unbLogo} width={56} alt="UnB Logo"/></h1>
       <h2>ULTIMATE TEAM</h2>
     </div>
   );
