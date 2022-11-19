af79e7d02c0cf2dd7807b648b0a775941642af79
guibrbs <200037994@aluno.unb.br>
2022-11-18 21:22:56 -0300

    build: create the project

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>PA 2022<span /></h1>
+      <h2>ULTIMATE TEAM</h2>
+    </div>
+  );
+}
+
+export default App;
