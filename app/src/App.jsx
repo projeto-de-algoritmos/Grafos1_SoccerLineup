e05ab8e78b918c320d323e1a5a5f80ef3b17ace7
guibrbs <200037994@aluno.unb.br>
2022-11-18 21:42:57 -0300

    feat: add soccer field

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import unbLogo from './assets/logo_unb.png'
+import soccerField from './assets/soccer_field.png'
 
 function App() {
   return (
     <div className="App">
       <h1>PA 2022 <img src={unbLogo} width={56} alt="UnB Logo"/></h1>
       <h2>ULTIMATE TEAM</h2>
+      <img src={soccerField} alt="Campo de futebol" className='soccer-field'/>
     </div>
   );
 }
