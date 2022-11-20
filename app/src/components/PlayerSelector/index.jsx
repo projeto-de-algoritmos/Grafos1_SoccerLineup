3300d75b51a9ec0882d4bafa1266bc108b8641ef
guibrbs <200037994@aluno.unb.br>
2022-11-20 16:17:53 -0300

    feat: create filter position to select players

--- app/src/components/PlayerSelector/index.jsx
+++ app/src/components/PlayerSelector/index.jsx
@@ -6,8 +6,32 @@ export default function PlayerSelector(props) {
     top: props.x,
     transform: "translate(-50%, -50%)",
   };
+
+  const handleClick = () => {
+    props.setOpenPlayerPicker(true)
+    switch (props.position){
+      case 'GOL':
+        props.setFilterPosition('Goalkeaper');
+        break;
+      case 'ZAG':
+      case 'LD':
+      case 'LE':
+        props.setFilterPosition('Defender');
+        break;
+      case 'MEI':
+        props.setFilterPosition('Midfielder');
+        break;
+      case 'CA':
+      case 'PD':
+      case 'PE':
+        props.setFilterPosition('Attacker');
+        break;
+      default:
+        props.setFilterPosition('')
+    }
+  }
   return (
-    <button className="player-selector-container" style={style} onClick={() => props.setOpenPlayerPicker(true)}>
+    <button className="player-selector-container" style={style} onClick={handleClick}>
       {props.team ? <></> : <p className="player-position">{props.position}</p>}
     </button>
   );
--- app/src/components/SoccerField/index.jsx
+++ app/src/components/SoccerField/index.jsx
@@ -6,11 +6,12 @@ import "./index.css";
 
 export default function SoccerField() {
   const [openPlayerPicker, setOpenPlayerPicker] = useState(false);
+  const [filterPosition, setFilterPosition] = useState('');
   return (
     <div className="field-container">
       {openPlayerPicker && (
         <>
-          <PlayerPicker setOpenPlayerPicker={setOpenPlayerPicker} />
+          <PlayerPicker setOpenPlayerPicker={setOpenPlayerPicker} filterPosition={filterPosition}/>
           <div className="backdrop" />
         </>
       )}
@@ -19,66 +20,77 @@ export default function SoccerField() {
         y="850px"
         position="CA"
         setOpenPlayerPicker={setOpenPlayerPicker}
+        setFilterPosition={setFilterPosition}
       />
       <PlayerSelector
         x="15%"
         y="740px"
         position="PD"
         setOpenPlayerPicker={setOpenPlayerPicker}
+        setFilterPosition={setFilterPosition}
       />
       <PlayerSelector
         x="85%"
         y="740px"
         position="PE"
         setOpenPlayerPicker={setOpenPlayerPicker}
+        setFilterPosition={setFilterPosition}
       />
       <PlayerSelector
         x="75%"
         y="480px"
         position="MEI"
         setOpenPlayerPicker={setOpenPlayerPicker}
+        setFilterPosition={setFilterPosition}
       />
       <PlayerSelector
         x="25%"
         y="480px"
         position="MEI"
         setOpenPlayerPicker={setOpenPlayerPicker}
+        setFilterPosition={setFilterPosition}
       />
       <PlayerSelector
         x="50%"
         y="350px"
         position="MEI"
         setOpenPlayerPicker={setOpenPlayerPicker}
+        setFilterPosition={setFilterPosition}
       />
       <PlayerSelector
         x="90%"
         y="190px"
         position="LE"
         setOpenPlayerPicker={setOpenPlayerPicker}
+        setFilterPosition={setFilterPosition}
       />
       <PlayerSelector
         x="10%"
         y="190px"
         position="LD"
         setOpenPlayerPicker={setOpenPlayerPicker}
+        setFilterPosition={setFilterPosition}
       />
       <PlayerSelector
         x="65%"
         y="140px"
         position="ZAG"
         setOpenPlayerPicker={setOpenPlayerPicker}
+        setFilterPosition={setFilterPosition}
       />
       <PlayerSelector
         x="35%"
         y="140px"
         position="ZAG"
         setOpenPlayerPicker={setOpenPlayerPicker}
+        setFilterPosition={setFilterPosition}
       />
       <PlayerSelector
         x="50%"
         y="0"
         position="GOL"
         setOpenPlayerPicker={setOpenPlayerPicker}
+        setFilterPosition={setFilterPosition}
       />
       <img src={soccerField} alt="Campo de futebol" className="soccer-field" />
     </div>
