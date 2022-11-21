6a03629b87cba6a866790bbc8b9cb22e6f688c9f
guibrbs <200037994@aluno.unb.br>
2022-11-21 20:53:09 -0300

    refactor: bfs visited logic

--- app/src/utils/bfsSearch.js
+++ app/src/utils/bfsSearch.js
@@ -1,15 +1,23 @@
 export default function bfsSearch(graph){
     var queue = [0]
-
+    var players_compatibility = 0
+    var visited = {}
     while(queue.length > 0){
         var currentNode = queue[0];
         queue.shift();
         for (var nextIndex = 0; nextIndex < graph[currentNode].next_pos.length; nextIndex++){
-            if(!graph[currentNode].visited){
-                graph[currentNode].next_pos.map((node) => queue.push(node));
-                graph[currentNode].visited = true;
+            if(!visited[`${currentNode},${graph[currentNode].next_pos[nextIndex]}`]){
+                if(graph[currentNode].players.nationality === graph[graph[currentNode].next_pos[nextIndex]].players.nationality){
+                    players_compatibility++;
+                }
+                if(graph[currentNode].players.team_name === graph[graph[currentNode].next_pos[nextIndex]].players.team_name){
+                    players_compatibility++;
+                }
+                queue.push(graph[currentNode].next_pos[nextIndex])
+                visited[`${currentNode},${graph[currentNode].next_pos[nextIndex]}`] = true
+                visited[`${graph[currentNode].next_pos[nextIndex]},${currentNode}`] = true
             }
         }
-        console.log(queue)
     }
+    return players_compatibility
 }
