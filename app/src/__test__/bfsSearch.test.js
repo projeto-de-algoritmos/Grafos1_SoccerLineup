88c43cb51df569c9b74c28d492f008e96ab7ccc0
guibrbs <200037994@aluno.unb.br>
2022-11-21 20:57:19 -0300

    test: assert team compatibility sum

--- app/src/__test__/bfsSearch.test.js
+++ app/src/__test__/bfsSearch.test.js
@@ -2,106 +2,95 @@ import bfsSearch from "../utils/bfsSearch";
 
 const providedGraph = [
 	{
-		"name": "L. Hughes",
-		"nationality": "Northern Ireland",
-		"team":{
-			"name": "Liverpool"
+		"players":{
+			"name": "L. Hughes",
+			"nationality": "Northern Ireland",
+			"team_name": "Liverpool",
 		},
 		"next_pos": [1,2],
-        "visited": false
 	},
 	{
-		"name": "Malachi Michael Fagan-Walcott",
-		"nationality": "England",
-		"team":{
-			"name": "Tottenham"
+		"players":{
+			"name": "Malachi Michael Fagan-Walcott",
+			"nationality": "Northern Ireland",
+			"team_name": "Liverpool",
 		},
 		"next_pos": [0, 2, 3, 5],
-        "visited": false
 	},
 	{
-		"name": "Björn Bryan Hardley",
-		"nationality": "Netherlands",
-		"team":{
-			"name": "Manchester United",
+		"players":{
+			"name": "Björn Bryan Hardley",
+			"nationality": "Northern Ireland",
+			"team_name": "Liverpool",
 		},
-		"next_pos": [0, 2, 4, 5],
-        "visited": false
+		"next_pos": [0, 1, 4, 5],
 	},
 	{
-		"name": "L. Hughes",
-		"nationality": "Northern Ireland",
-		"team":{
-			"name": "Liverpool"
+		"players":{
+			"name": "L. Hughes",
+			"nationality": "Northern Ireland",
+			"team_name": "Liverpool",
 		},
 		"next_pos": [1, 6],
-        "visited": false
 	},
 	{
-		"name": "J. Simmons",
-		"nationality": "Netherlands",
-		"team":{
-			"name": "Manchester United"
+		"players":{
+			"name": "J. Simmons",
+			"nationality": "Northern Ireland",
+			"team_name": "Liverpool",
 		},
 		"next_pos": [2, 7],
-        "visited": false
 	},
 	{
-		"name": "K. Jabi",
-		"nationality": "Sweeden",
-		"team":{
-			"name": "Manchester United"
+		"players":{
+			"name": "K. Jabi",
+			"nationality": "Northern Ireland",
+			"team_name": "Liverpool",
 		},
 		"next_pos": [1, 2, 6, 7, 8],
-        "visited": false
 	},
 	{
-		"name": "M. Joash",
-		"nationality": "England",
-		"team":{
-			"name": "Liverpool"
+		"players":{
+			"name": "M. Joash",
+			"nationality": "Northern Ireland",
+			"team_name": "Liverpool",
 		},
-		"next_pos": [3, 5],
-        "visited": false
+		"next_pos": [3, 5, 9],
 	},
 	{
-		"name": "James Storer",
-		"nationality": "England",
-		"team":{
-			"name": "Wolves"
+		"players":{
+			"name": "James Storer",
+			"nationality": "Northern Ireland",
+			"team_name": "Liverpool",
 		},
-		"next_pos": [4, 5],
-        "visited": false
+		"next_pos": [4, 5, 10],
 	},
 	{
-		"name": "Jacob Bunch",
-		"nationality": "England",
-		"team":{
-			"name": "Manchester United"
+		"players":{
+			"name": "Jacob Bunch",
+			"nationality": "Northern Ireland",
+			"team_name": "Liverpool",
 		},
 		"next_pos": [5, 9, 10],
-        "visited": false
 	},
 	{
-		"name": "K. Bale",
-		"nationality": "Northern Ireland",
-		"team":{
-			"name": "Manchester United"
+		"players":{
+			"name": "K. Bale",
+			"nationality": "Northern Ireland",
+			"team_name": "Liverpool",
 		},
 		"next_pos": [6, 8],
-        "visited": false
 	},
 	{
-		"name": "Jamie Patrick Bowden",
-		"nationality": "Sweeden",
-		"team":{
-			"name": "Republic of Ireland"
+		"players":{
+			"name": "Jamie Patrick Bowden",
+			"nationality": "Northern Ireland",
+			"team_name": "Liverpool",
 		},
 		"next_pos": [7, 8],
-        "visited": false
 	},
 ]
 
-it('should console', () => {
-    bfsSearch(providedGraph)
+it('should match players compability sum', () => {
+    expect(bfsSearch(providedGraph)).toBe(32)
 })
