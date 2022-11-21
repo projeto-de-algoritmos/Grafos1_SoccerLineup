c2b9e4235832d2d3cee663f429ec3b401d454a8b
guibrbs <200037994@aluno.unb.br>
2022-11-21 20:59:04 -0300

    feat: use bfs search passing the players graph selected by user

--- app/src/components/SoccerField/index.jsx
+++ app/src/components/SoccerField/index.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { useState } from "react";
 import soccerField from "../../assets/soccer_field.png";
 import { PlayerContext } from "../../contexts/PlayersContext";
+import bfsSearch from "../../utils/bfsSearch";
 import PlayerPicker from "../PlayerPicker";
 import PlayerSelector from "../PlayerSelector";
 import "./index.css";
@@ -9,119 +10,129 @@ import "./index.css";
 export default function SoccerField() {
   const [openPlayerPicker, setOpenPlayerPicker] = useState({
     state: false,
-    positionIndex: 0
+    positionIndex: 0,
   });
-  const [filterPosition, setFilterPosition] = useState('');
-  const {playersGraph} = useContext(PlayerContext)
-
+  const [filterPosition, setFilterPosition] = useState("");
+  const { playersGraph } = useContext(PlayerContext);
   return (
-    <div className="field-container">
-      {openPlayerPicker.state && (
-        <>
-          <PlayerPicker setOpenPlayerPicker={setOpenPlayerPicker} positionIndex={openPlayerPicker.positionIndex} filterPosition={filterPosition}/>
-          <div className="backdrop" />
-        </>
-      )}
-      <PlayerSelector
-        x="50%"
-        y="850px"
-        position="CA"
-        setOpenPlayerPicker={setOpenPlayerPicker}
-        setFilterPosition={setFilterPosition}
-        playerData={playersGraph[10]}
-        index={10}
-      />
-      <PlayerSelector
-        x="15%"
-        y="740px"
-        position="PD"
-        setOpenPlayerPicker={setOpenPlayerPicker}
-        setFilterPosition={setFilterPosition}
-        playerData={playersGraph[9]}
-        index={9}
-      />
-      <PlayerSelector
-        x="85%"
-        y="740px"
-        position="PE"
-        setOpenPlayerPicker={setOpenPlayerPicker}
-        setFilterPosition={setFilterPosition}
-        playerData={playersGraph[8]}
-        index={8}
-      />
-      <PlayerSelector
-        x="75%"
-        y="480px"
-        position="MEI"
-        setOpenPlayerPicker={setOpenPlayerPicker}
-        setFilterPosition={setFilterPosition}
-        playerData={playersGraph[7]}
-        index={7}
-      />
-      <PlayerSelector
-        x="25%"
-        y="480px"
-        position="MEI"
-        setOpenPlayerPicker={setOpenPlayerPicker}
-        setFilterPosition={setFilterPosition}
-        playerData={playersGraph[6]}
-        index={6}
-      />
-      <PlayerSelector
-        x="50%"
-        y="350px"
-        position="MEI"
-        setOpenPlayerPicker={setOpenPlayerPicker}
-        setFilterPosition={setFilterPosition}
-        playerData={playersGraph[5]}
-        index={5}
-      />
-      <PlayerSelector
-        x="90%"
-        y="190px"
-        position="LE"
-        setOpenPlayerPicker={setOpenPlayerPicker}
-        setFilterPosition={setFilterPosition}
-        playerData={playersGraph[4]}
-        index={4}
-      />
-      <PlayerSelector
-        x="10%"
-        y="190px"
-        position="LD"
-        setOpenPlayerPicker={setOpenPlayerPicker}
-        setFilterPosition={setFilterPosition}
-        playerData={playersGraph[3]}
-        index={3}
-      />
-      <PlayerSelector
-        x="65%"
-        y="140px"
-        position="ZAG"
-        setOpenPlayerPicker={setOpenPlayerPicker}
-        setFilterPosition={setFilterPosition}
-        playerData={playersGraph[2]}
-        index={2}
-      />
-      <PlayerSelector
-        x="35%"
-        y="140px"
-        position="ZAG"
-        setOpenPlayerPicker={setOpenPlayerPicker}
-        setFilterPosition={setFilterPosition}
-        playerData={playersGraph[1]}
-        index={1}
-      />
-      <PlayerSelector
-        x="50%"
-        y="0"
-        position="GOL"
-        setOpenPlayerPicker={setOpenPlayerPicker}
-        setFilterPosition={setFilterPosition}
-        playerData={playersGraph[0]}
-        index={0}
-      />
-      <img src={soccerField} alt="Campo de futebol" className="soccer-field" />
-    </div>
+    <>
+      <div className="field-container">
+        {openPlayerPicker.state && (
+          <>
+            <PlayerPicker
+              setOpenPlayerPicker={setOpenPlayerPicker}
+              positionIndex={openPlayerPicker.positionIndex}
+              filterPosition={filterPosition}
+            />
+            <div className="backdrop" />
+          </>
+        )}
+        <PlayerSelector
+          x="50%"
+          y="850px"
+          position="CA"
+          setOpenPlayerPicker={setOpenPlayerPicker}
+          setFilterPosition={setFilterPosition}
+          playerData={playersGraph[10]}
+          index={10}
+        />
+        <PlayerSelector
+          x="15%"
+          y="740px"
+          position="PD"
+          setOpenPlayerPicker={setOpenPlayerPicker}
+          setFilterPosition={setFilterPosition}
+          playerData={playersGraph[9]}
+          index={9}
+        />
+        <PlayerSelector
+          x="85%"
+          y="740px"
+          position="PE"
+          setOpenPlayerPicker={setOpenPlayerPicker}
+          setFilterPosition={setFilterPosition}
+          playerData={playersGraph[8]}
+          index={8}
+        />
+        <PlayerSelector
+          x="75%"
+          y="480px"
+          position="MEI"
+          setOpenPlayerPicker={setOpenPlayerPicker}
+          setFilterPosition={setFilterPosition}
+          playerData={playersGraph[7]}
+          index={7}
+        />
+        <PlayerSelector
+          x="25%"
+          y="480px"
+          position="MEI"
+          setOpenPlayerPicker={setOpenPlayerPicker}
+          setFilterPosition={setFilterPosition}
+          playerData={playersGraph[6]}
+          index={6}
+        />
+        <PlayerSelector
+          x="50%"
+          y="350px"
+          position="MEI"
+          setOpenPlayerPicker={setOpenPlayerPicker}
+          setFilterPosition={setFilterPosition}
+          playerData={playersGraph[5]}
+          index={5}
+        />
+        <PlayerSelector
+          x="90%"
+          y="190px"
+          position="LE"
+          setOpenPlayerPicker={setOpenPlayerPicker}
+          setFilterPosition={setFilterPosition}
+          playerData={playersGraph[4]}
+          index={4}
+        />
+        <PlayerSelector
+          x="10%"
+          y="190px"
+          position="LD"
+          setOpenPlayerPicker={setOpenPlayerPicker}
+          setFilterPosition={setFilterPosition}
+          playerData={playersGraph[3]}
+          index={3}
+        />
+        <PlayerSelector
+          x="65%"
+          y="140px"
+          position="ZAG"
+          setOpenPlayerPicker={setOpenPlayerPicker}
+          setFilterPosition={setFilterPosition}
+          playerData={playersGraph[2]}
+          index={2}
+        />
+        <PlayerSelector
+          x="35%"
+          y="140px"
+          position="ZAG"
+          setOpenPlayerPicker={setOpenPlayerPicker}
+          setFilterPosition={setFilterPosition}
+          playerData={playersGraph[1]}
+          index={1}
+        />
+        <PlayerSelector
+          x="50%"
+          y="0"
+          position="GOL"
+          setOpenPlayerPicker={setOpenPlayerPicker}
+          setFilterPosition={setFilterPosition}
+          playerData={playersGraph[0]}
+          index={0}
+        />
+        <img
+          src={soccerField}
+          alt="Campo de futebol"
+          className="soccer-field"
+        />
+      </div>
+      <button onClick={() => bfsSearch(playersGraph)}>Validar time</button>
+    </>
   );
 }
